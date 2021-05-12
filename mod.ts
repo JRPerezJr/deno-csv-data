1c3dfca325724e074c3f322b850ba588a0eb8591
Juan <pront23@gmail.com>
2021-05-12 14:34:12 +0900

    imported lodash

--- mod.ts
+++ mod.ts
@@ -1,6 +1,7 @@
 import { join } from 'https://deno.land/std/path/mod.ts';
 import { BufReader } from 'https://deno.land/std/io/bufio.ts';
 import { parse } from 'https://deno.land/std/encoding/csv.ts';
+import * as _ from 'https://raw.githubusercontent.com/lodash/lodash/4.17.21-es/lodash.js';
 
 interface Planet {
   [key: string]: string;
