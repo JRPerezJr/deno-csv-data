f043cbc61c828abfff7e137221ff62b22ddd5673
Juan <pront23@gmail.com>
2021-05-12 16:16:12 +0900

    integrated new functions

--- mod.ts
+++ mod.ts
@@ -1,11 +1,9 @@
 import { join } from 'https://deno.land/std/path/mod.ts';
 import { BufReader } from 'https://deno.land/std/io/bufio.ts';
 import { parse } from 'https://deno.land/std/encoding/csv.ts';
-import * as _ from 'https://deno.land/x/lodash@4.17.15-es/lodash.js';
+import { findHabitablePlanets } from './habitable_planets.ts';
+import { getPlanetData } from './habitable_planet_data.ts';
 
-interface Planet {
-  [key: string]: string;
-}
 async function loadPlanetsData() {
   const path = join('.', 'kepler_exoplanets_data.csv');
 
@@ -17,34 +15,12 @@ async function loadPlanetsData() {
   });
   Deno.close(file.rid);
 
-  const planets = (result as Array<Planet>).filter((planet) => {
-    const planetaryRadius = Number(planet['koi_prad']);
-    const stellarMass = Number(planet['koi_smass']);
-    const stellarRadius = Number(planet['koi_srad']);
+  const habitablePlanets = findHabitablePlanets(result);
 
-    return (
-      planet['koi_disposition'] === 'CONFIRMED' &&
-      planetaryRadius > 0.5 &&
-      planetaryRadius < 1.5 &&
-      stellarMass > 0.78 &&
-      stellarMass < 1.04 &&
-      stellarRadius > 0.99 &&
-      stellarRadius < 1.01
-    );
-  });
-
-  return planets.map((planet) => {
-    return _.pick(planet, [
-      'koi_prad',
-      'koi_smass',
-      'koi_srad',
-      'kepler_name',
-      'koi_count',
-      'koi_steff',
-    ]);
-  });
+  return getPlanetData(habitablePlanets);
 }
 const newEarths = await loadPlanetsData();
+
 for (const planet of newEarths) {
   console.log(planet);
 }
